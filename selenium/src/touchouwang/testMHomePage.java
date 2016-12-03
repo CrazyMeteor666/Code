02d48247b138b6c5aba50fb9dc7430db5a4406f1
CrazyMeteor <121651507@qq.com>
2016-12-03 15:46:02 +0800

    2016年12月3日 15:45:53
    例行提交

--- selenium/src/touchouwang/testMHomePage.java
+++ selenium/src/touchouwang/testMHomePage.java
@@ -33,6 +33,41 @@ public class testMHomePage {
 		log.write("移动端首页\r\n");
 
 		//case1：banner、列表图片加载
+		log.write("case1：banner、列表图片加载\r\n");
+		String banner=driver.findElement(By.xpath("//*[@id='swiper']/div[1]/div[3]/a/img")).getAttribute("src");
+		URL banner_url=new URL(banner);
+		try {
+			HttpURLConnection uConnection = (HttpURLConnection) banner_url.openConnection();
+			if (uConnection.getResponseCode()==200) {
+				log.write(time.format(new Date())+"   首页banner加载成功！\r\n");
+			}
+			else {
+				log.write("报错：首页banner加载失败！\r\n");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for (int i = 1; i < 4; i++) {
+			String xpath1="/html/body/article[";
+			String xpath2=i+"";
+			String xpath3="]/div[2]/img";
+			String newxpath=xpath1+xpath2+xpath3;
+			String list_pic1=driver.findElement(By.xpath(newxpath)).getAttribute("src");
+			URL list_pic1_url=new URL(list_pic1);
+			try {
+				HttpURLConnection uConnection2=(HttpURLConnection) list_pic1_url.openConnection();
+				if (uConnection2.getResponseCode()==200) {
+					log.write(time.format(new Date())+"   首页项目列表第"+i+"张图加载成功\r\n");
+				} else {
+					log.write("报错：首页项目列表第"+i+"张图加载失败\r\n");
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 		
 	}
 }
